cce0d4551fc4ec66beddbbfce6c0ff0096e0cf50
OksanaKis <oksanakisetoja@gmail.com>
2022-02-16 11:04:13 +0200

    creating correct search input

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -10,7 +10,6 @@ function Sidebar() {
   const [rooms, setRooms] = useState([]);
   const [{ user }, dispatch] = useStateValue();
   const [value, setValue] = useState("");
-  const [isOpen, setIsOpen] = useState(true);
 
   useEffect(() => {
     const unsubscribe = db.collection("rooms").onSnapshot((snapshot) => {
@@ -30,15 +29,6 @@ function Sidebar() {
     return chatName.data.name.toLowerCase().includes(value.toLowerCase());
   });
 
-  const itemClickHandler = (e) => {
-    setValue(e.target.textContent);
-    setIsOpen(!isOpen);
-  };
-
-  const inputClickHandler = () => {
-    setIsOpen(true);
-  };
-
   return (
     <div className="sidebar">
       <div className="sidebar_header">
@@ -50,30 +40,13 @@ function Sidebar() {
           <input
             type="text"
             placeholder="Search or start new chat"
-            value={value}
             onChange={(event) => setValue(event.target.value)}
-            onClick={inputClickHandler}
           />
-          <ul className="searchInput">
-            {value && isOpen
-              ? filteredChatNames.map((person, index) => {
-                  return (
-                    <li
-                      key={index}
-                      className="searchInputItem"
-                      onClick={itemClickHandler}
-                    >
-                      {person.data.name}
-                    </li>
-                  );
-                })
-              : null}
-          </ul>
         </div>
       </div>
       <div className="sidebar_chats">
         <h2>Chats</h2>
-        {rooms.map((room) => (
+        {filteredChatNames.map((room) => (
           <SidebarChat key={room.id} id={room.id} name={room.data.name} />
         ))}
       </div>
