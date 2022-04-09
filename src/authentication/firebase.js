866d33ed502755a9489a8a5d0a7703727482c986
OksanaKis <oksanakisetoja@gmail.com>
2022-04-09 13:09:21 +0200

    deploy the project

--- src/authentication/firebase.js
+++ src/authentication/firebase.js
@@ -3,16 +3,6 @@ import "firebase/compat/auth";
 import "firebase/compat/firestore";
 import "firebase/compat/storage";
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyAzhN-SQp9wR7W9tcf3ybM0a-gGsYhPY44",
-//   authDomain: "chat-app-7c113.firebaseapp.com",
-//   projectId: "chat-app-7c113",
-//   storageBucket: "chat-app-7c113.appspot.com",
-//   messagingSenderId: "263079939697",
-//   appId: "1:263079939697:web:266999b1662fdb5b2655d0",
-//   measurementId: "G-5MBDC1BC48",
-// };
-
 const firebaseConfig = {
   apiKey: "AIzaSyBwbG7HM1G6Xh6_cZfw3L3K-aXawdK9_8I",
   authDomain: "real-chat-9d376.firebaseapp.com",
