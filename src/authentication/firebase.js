4f555c80d48d91b587a7edb4831c630693dccc67
OksanaKis <oksanakisetoja@gmail.com>
2023-08-11 10:52:14 -0400

    firebaseConfig

--- src/authentication/firebase.js
+++ src/authentication/firebase.js
@@ -3,6 +3,17 @@ import "firebase/compat/auth";
 import "firebase/compat/firestore";
 import "firebase/compat/storage";
 
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBO53R4g_dXsNJ29GaNXBQU3ec7Udgq6Gc",
+//   authDomain: "new-react-chat-app-831b4.firebaseapp.com",
+//   projectId: "new-react-chat-app-831b4",
+//   storageBucket: "new-react-chat-app-831b4.appspot.com",
+//   messagingSenderId: "25715960678",
+//   appId: "1:25715960678:web:100a6ad65a1e08923436a7",
+//   measurementId: "G-PNKNM9LNZ4",
+// };
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBO53R4g_dXsNJ29GaNXBQU3ec7Udgq6Gc",
   authDomain: "new-react-chat-app-831b4.firebaseapp.com",
@@ -10,7 +21,7 @@ const firebaseConfig = {
   storageBucket: "new-react-chat-app-831b4.appspot.com",
   messagingSenderId: "25715960678",
   appId: "1:25715960678:web:100a6ad65a1e08923436a7",
-  measurementId: "G-PNKNM9LNZ4",
+  measurementId: "G-PNKNM9LNZ4"
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
